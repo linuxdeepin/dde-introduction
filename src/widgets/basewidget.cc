330ee9959767b02b82f7fe7e55ff9e6fcba7c40b
haruyukilxz <kirigaya@mkacg.com>
2018-02-22 16:37:41 +0800

    feat: add base widget
    Change-Id: Icc6da1662133e6fcb6b30bf83802a138303990e0

---
+++ src/widgets/basewidget.cc
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2017 ~ 2018 Deepin Technology Co., Ltd.
+ *
+ * Author:     kirigaya <kirigaya@mkacg.com>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "basewidget.h"
+
+BaseWidget::BaseWidget(QWidget *parent)
+    : QFrame(parent)
+    , m_layout(new QVBoxLayout(this))
+    , m_title(new QLabel(this))
+    , m_borderWidget(new BorderWidget(this))
+{
+    m_layout->setMargin(0);
+    m_layout->setSpacing(0);
+
+    m_layout->addWidget(m_borderWidget, 0, Qt::AlignCenter);
+    m_layout->addSpacing(5);
+    m_layout->addWidget(m_title, 0, Qt::AlignCenter);
+
+    setLayout(m_layout);
+}
+
+void BaseWidget::setPixmap(const QPixmap &pixmap)
+{
+    m_borderWidget->setFixedSize(pixmap.size());
+    m_borderWidget->setPixmap(pixmap);
+
+    adjustSize();
+}
+
+void BaseWidget::setTitle(const QString &title)
+{
+    m_title->setText(title);
+}
+
+void BaseWidget::setChecked(const bool checked)
+{
+
+}
+
+void BaseWidget::mouseReleaseEvent(QMouseEvent *event)
+{
+    QFrame::mouseReleaseEvent(event);
+
+    emit clicked();
+}
---
+++ src/widgets/basewidget.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2017 ~ 2018 Deepin Technology Co., Ltd.
+ *
+ * Author:     kirigaya <kirigaya@mkacg.com>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef BASEWIDGET_H
+#define BASEWIDGET_H
+
+#include "borderwidget.h"
+#include <QFrame>
+#include <QVBoxLayout>
+#include <QLabel>
+
+class BaseWidget : public QFrame
+{
+    Q_OBJECT
+public:
+    explicit BaseWidget(QWidget *parent = nullptr);
+
+Q_SIGNALS:
+    void clicked();
+
+public Q_SLOTS:
+    void setPixmap(const QPixmap &pixmap);
+    void setTitle(const QString &title);
+    void setChecked(const bool checked);
+
+protected:
+    void mouseReleaseEvent(QMouseEvent *event) Q_DECL_OVERRIDE;
+
+private:
+    QVBoxLayout* m_layout;
+    QLabel* m_title;
+    BorderWidget* m_borderWidget;
+};
+
+#endif // BASEWIDGET_H
