855d98089987638e260fe34efba25e7ba9ac2661
haruyukilxz <kirigaya@mkacg.com>
2018-02-22 16:11:30 +0800

    feat: add borderwidget
    Change-Id: I77b3074ceae94d2760b4f67dfcd32e005933b13d

---
+++ src/widgets/borderwidget.cc
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2017 ~ 2018 Deepin Technology Co., Ltd.
+ *
+ * Author:     kirigaya <kirigaya@mkacg.com>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "borderwidget.h"
+#include <QPainter>
+#include <QPainterPath>
+#include <QDebug>
+
+BorderWidget::BorderWidget(QWidget *parent)
+    : QFrame(parent)
+{
+}
+
+void BorderWidget::setPixmap(const QPixmap &pixmap)
+{
+    m_pixmap = pixmap;
+
+    update();
+}
+
+void BorderWidget::paintEvent(QPaintEvent *event)
+{
+    QFrame::paintEvent(event);
+
+    QPainter painter(this);
+    painter.setRenderHint(QPainter::Antialiasing, true);
+
+    QPainterPath contentPath;
+    contentPath.addRoundedRect(rect().adjusted(4, 4, -4, -4), 5, 5);
+    painter.fillPath(contentPath, m_pixmap);
+
+    QPainterPath path;
+    path.addRoundedRect(rect().adjusted(2, 2, -2, -2), 5, 5);
+
+    QPen pen(QColor("#2CA7F8"));
+    pen.setWidth(2);
+
+    painter.setPen(pen);
+    painter.drawPath(path);
+}
