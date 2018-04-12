b77182fcccb5d648c33eb154cda1a5b0550408b8
haruyukilxz <kirigaya@mkacg.com>
2018-04-12 11:14:15 +0800

    add translation
    Change-Id: Ifddb690c41a17f60f0b8352c43df7d0477a5d92d

---
+++ translations/dde-introduction.ts
@@ -0,0 +1,162 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../src/modules/about.cc" line="22"/>
+        <source>&lt;body&gt;                       &lt;b&gt;About us&lt;/b&gt;                        &lt;br&gt; deepin is a Linux distribution committed to providing elegant, user-friendly, safe and stable experience for users all                       over the world. It not only integrates and configures the best open-source products, but also develops a series of applications                       for daily use, such as Deepin Store, Deepin Screenshot, Deepin Music, Deepin Movie and so on, among which Deepin Desktop                       Environment and Deepin Control Center are based on Qt technology. deepin focuses on user-friendly experience and beautiful                       design, so it’s easy to install and use for most users and can well replace Windows for work and entertainment.                       &lt;br&gt;                       &lt;b&gt;Objective&lt;/b&gt;                       &lt;br&gt;                       &lt;li&gt;Customer First&lt;/li&gt;                       Deepin Technology takes user requirements as core, adheres to innovative idea, and bases on the market demand to feedback                       users with better products and services.                       &lt;br&gt;                       &lt;li&gt;Open Source Idea&lt;/li&gt;                       The deepin project is a creative and energetic open source desktop operating system. It insists on the open source idea always,                       the development are under GPL License and all codes are open source, which has been uploaded to GitHub. Community members,                       open source fans and Deepin Technology will develop the deepin project together to make a due contribution to China open                       source.                       &lt;li&gt;Open and Free&lt;/li&gt;                       Only in a comfortable, pleasure and free working environment, one can get high work efficiency. In Deepin Technology, no                       long and boring meeting and workflow, no complicated class hierarchy, all are equal and cooperative. It provides regular                       professional training, prospective career development and mature promote channel for employees.                       &lt;br&gt;                       &lt;b&gt;source project deepin&lt;/b&gt;                       &lt;br&gt; is an open source desktop system developed for common users. You are free to download, distribute, modify and use it.                       Detailed address is GitHub: https://github.com/linuxdeepin                       &lt;/body&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>BottomNavigation</name>
+    <message>
+        <location filename="../src/widgets/bottomnavigation.cc" line="29"/>
+        <source>Official website</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/bottomnavigation.cc" line="32"/>
+        <source>Community</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/bottomnavigation.cc" line="35"/>
+        <source>Feedback</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/bottomnavigation.cc" line="38"/>
+        <source>Acknowledgements</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DesktopModeModule</name>
+    <message>
+        <location filename="../src/modules/desktopmodemodule.cc" line="27"/>
+        <source>Efficient Mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/desktopmodemodule.cc" line="28"/>
+        <source>Fashion Mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/mainwindow.cc" line="115"/>
+        <source>next</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cc" line="219"/>
+        <source>Please select desktop mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cc" line="220"/>
+        <source>You can switch it in Mode by right clicking on dock</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cc" line="231"/>
+        <source>Please select to enable window effect or not</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cc" line="232"/>
+        <source>You can enable or disable it in Control Center &gt; Personalization &gt; Enable window effect, or use shortcuts Shift+Super+Tab</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cc" line="243"/>
+        <source>Please select icon theme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cc" line="244"/>
+        <source>You can change it in Control Center &gt; Personalization &gt; Theme &gt; Icon Theme</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>NormalModule</name>
+    <message>
+        <location filename="../src/modules/normalmodule.cc" line="109"/>
+        <source>Introduction</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/normalmodule.cc" line="110"/>
+        <source>Desktop mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/normalmodule.cc" line="111"/>
+        <source>Icon theme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/normalmodule.cc" line="112"/>
+        <source>Window effect</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/normalmodule.cc" line="113"/>
+        <location filename="../src/modules/normalmodule.cc" line="122"/>
+        <source>Support us</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/normalmodule.cc" line="114"/>
+        <location filename="../src/modules/normalmodule.cc" line="123"/>
+        <source>About us</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/normalmodule.cc" line="118"/>
+        <source>Welcome</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/normalmodule.cc" line="119"/>
+        <source>Please select desktop mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/normalmodule.cc" line="120"/>
+        <source>Please select icon theme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/normalmodule.cc" line="121"/>
+        <source>Please select to enable window effect or not</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Support</name>
+    <message>
+        <location filename="../src/modules/support.cc" line="22"/>
+        <source>&lt;body&gt;                       &lt;b&gt;Internationalization&lt;/b&gt;                       &lt;br&gt; deepin was born in China and aimed to go global. So as to make it possible for more and more users from around the world                       to experience the Linux distribution from China, let the system widely spread all over the world and enlarge its global                       impact, we hope that more and more people from the Linux community can join to help the translation work to let our open                       source work benefit the world! The i18n translation work is mainly about: deepin: Joining the document writing and i18n                       work to let our open source work benefit the world! Deepin community: Providing community news, download, mirrors, projects                       and a gathering place for developers! Welcome to join i18n project.                       &lt;br&gt;                       &lt;b&gt;Donations&lt;/b&gt;                       &lt;br&gt; The development of deepin can not move on without community support, for further improve the construction of the deepin                       community environment, Deepin Technology has made the decision to officially open channels of donation. The donation                       upholds the principle of completely voluntary and no matter who you are, wherever you are, how much you contribute, are                       all making contributions to the development of Deepin community and we are very grateful for your donation. The donation                       aims to make a survey and decide the orientation and proirity of the projects, support the continuous further development,                       and support community activities and construction.                       &lt;br&gt;                       &lt;b&gt;Internal Testing&lt;/b&gt;                       &lt;br&gt; For quality assurance of the deepin project, we welcome more people to participate in the internal testing and feedback                       tracking. Become a Internal Testing Team member, you will                       &lt;li&gt;Get the latest achievements of deepin and software timely.&lt;/li&gt;                       &lt;li&gt;Obtain the latest development plans and progress of deepin project in advance.&lt;/li&gt;                       &lt;li&gt;Be on deepin contributors list.&lt;/li&gt;                       &lt;li&gt;Outstanding members will get a souvenir of deepin.&lt;/li&gt;                       &lt;/body&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WMModeModule</name>
+    <message>
+        <location filename="../src/modules/wmmodemodule.cc" line="27"/>
+        <source>Efficient Mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/wmmodemodule.cc" line="28"/>
+        <source>Fashion Mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
