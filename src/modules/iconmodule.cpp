c61df69eeb58e32fb787a76875bcc1c3a0ae0ab4
haruyukilxz <kirigaya@mkacg.com>
2018-05-21 15:14:32 +0800

    feat(icon): set vertical spacing
    Change-Id: If49832307d5af71af9b7243fd27ca37d46bb3a95

--- src/modules/iconmodule.cpp
+++ src/modules/iconmodule.cpp
@@ -44,6 +44,7 @@ IconModule::IconModule(QWidget *parent)
     m_layout->setSpacing(0);
     m_layout->setMargin(0);
     m_layout->setContentsMargins(15, 8, 20, -10);
+    m_layout->setVerticalSpacing(30);
 
     m_selectBtn->setParent(m_scrollWidget);
 
