331c4228888d24601c5f3129b8ab066a1a162a72
Zheng Youge <youge.zheng@deepin.io>
2019-09-12 22:04:03 +0800

    chore:fix theme config file location

--- src/main.cpp
+++ src/main.cpp
@@ -22,12 +22,62 @@
 #include <QDebug>
 #include <DWidgetUtil>
 #include <DPlatformWindowHandle>
+#include <DWidget>
+#include <QDBusInterface>
+#include <DWindowManagerHelper>
+#include <DGuiApplicationHelper>
 
 #ifndef DISABLE_VIDEO
 #include <compositing_manager.h>
 #endif
 
 DWIDGET_USE_NAMESPACE
+
+static QString g_appPath;//全局路径
+
+//获取配置文件主题类型，并重新设置
+DGuiApplicationHelper::ColorType getThemeTypeSetting()
+{
+    //需要找到自己程序的配置文件路径，并读取配置，这里只是用home路径下themeType.cfg文件举例,具体配置文件根据自身项目情况
+    QString t_appDir = g_appPath + QDir::separator() + "themetype.cfg";
+    QFile t_configFile(t_appDir);
+
+    t_configFile.open(QIODevice::ReadOnly | QIODevice::Text);
+    QByteArray t_readBuf = t_configFile.readAll();
+    int t_readType = QString(t_readBuf).toInt();
+
+    //获取读到的主题类型，并返回设置
+    switch (t_readType) {
+    case 0:
+        // 跟随系统主题
+        return DGuiApplicationHelper::UnknownType;
+    case 1:
+//        浅色主题
+        return DGuiApplicationHelper::LightType;
+
+    case 2:
+//        深色主题
+        return DGuiApplicationHelper::DarkType;
+    default:
+        // 跟随系统主题
+        return DGuiApplicationHelper::UnknownType;
+    }
+
+}
+
+//保存当前主题类型配置文件
+void saveThemeTypeSetting(int type)
+{
+    //需要找到自己程序的配置文件路径，并写入配置，这里只是用home路径下themeType.cfg文件举例,具体配置文件根据自身项目情况
+    QString t_appDir = g_appPath + QDir::separator() + "themetype.cfg";
+    QFile t_configFile(t_appDir);
+
+    t_configFile.open(QIODevice::WriteOnly | QIODevice::Text);
+    //直接将主题类型保存到配置文件，具体配置key-value组合根据自身项目情况
+    QString t_typeStr = QString::number(type);
+    t_configFile.write(t_typeStr.toUtf8());
+    t_configFile.close();
+}
 
 int main(int argc, char *argv[])
 {
@@ -37,6 +87,11 @@ int main(int argc, char *argv[])
 
     DApplication::loadDXcbPlugin();
     DApplication a(argc, argv);
+
+    g_appPath = QDir::homePath() + QDir::separator() + "." + qApp->applicationName();
+    QDir t_appDir;
+    t_appDir.mkpath(g_appPath);
+
     a.setApplicationName("dde-introduction");
 
     // dapplication default setting is true
@@ -50,6 +105,17 @@ int main(int argc, char *argv[])
     a.setOrganizationName("deepin");
     a.setApplicationVersion(DApplication::buildVersion("1.0"));
     a.loadTranslator();
+
+    // 应用已保存的主题设置
+    DGuiApplicationHelper::instance()->setPaletteType(getThemeTypeSetting());
+
+    //监听当前应用主题切换事件
+    QObject::connect(DGuiApplicationHelper::instance(), &DGuiApplicationHelper::paletteTypeChanged,
+    [] (DGuiApplicationHelper::ColorType type) {
+        qDebug() << type;
+        // 保存程序的主题设置  type : 0,系统主题， 1,浅色主题， 2,深色主题
+        saveThemeTypeSetting(type);
+    });
 
     static const QDate buildDate = QLocale( QLocale::English ).toDate( QString(__DATE__).replace("  ", " 0"), "MMM dd yyyy");
     QString t_date = buildDate.toString("MMdd");
